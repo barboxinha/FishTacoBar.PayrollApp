ac79f233339d6e0d5d7a0985259b9a3429b19581
Marco Barboza <Marco.Barboza@smithgroup.com>
2023-03-30 01:10:32 -0700

    Add project files.

---
+++ FishTacoBar.PayrollApp/Data/Staff.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishTacoBar.PayrollApp.Data
+{
+    public class Staff
+    {
+        private float _hourlyRate;
+        private int _hoursWorked;
+
+        public float TotalPay { get; protected set; }
+        public float BasicPay { get; private set; }
+        public string StaffName { get; private set; }
+
+        public int HoursWorked
+        {
+            get { return _hoursWorked; }
+            set 
+            {
+                _hoursWorked = value > 0 ? value : 0;
+            }
+        }
+
+        public Staff(string name, float rate)
+        {
+            StaffName = name;
+            _hourlyRate = rate;
+        }
+
+        public virtual void CalculatePay()
+        {
+            Console.WriteLine("Calculating Pay...");
+
+            BasicPay = _hoursWorked * _hourlyRate;
+            TotalPay = BasicPay;
+        }
+
+        public override string ToString()
+        {
+            return "\nStaffName = " + StaffName +
+                   "\n_houryRate = " + _hourlyRate +
+                   "\n_hoursWorked = " + _hoursWorked +
+                   "\nBasicPay = " + BasicPay +
+                   "\n\nTotalPay = " + TotalPay;
+        }
+    }
+}
---
+++ FishTacoBar.PayrollApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FishTacoBar.PayrollApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SmithGroup Inc.")]
+[assembly: AssemblyProduct("FishTacoBar.PayrollApp")]
+[assembly: AssemblyCopyright("Copyright © SmithGroup Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("60a9ed4e-319f-401f-943e-055af22ad83f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
