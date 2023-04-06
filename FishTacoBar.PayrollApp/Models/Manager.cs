12850db98032b5a06bcc8a2f06871d088f31c3d8
Marco Barboza <Marco.Barboza@smithgroup.com>
2023-04-06 00:45:04 -0700

    Add manager class.

---
+++ FishTacoBar.PayrollApp/Models/Manager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishTacoBar.PayrollApp.Models
+{
+    public class Manager : Staff
+    {
+        private const float _managerHourlyRate = 60;
+        public int Allowance { get; private set; }
+
+        public Manager(string name) : base(name, _managerHourlyRate)
+        {
+        }
+
+        public override void CalculatePay()
+        {
+            base.CalculatePay();
+            Allowance = 1000;
+
+            if (HoursWorked > 160)
+            {
+                TotalPay = BasicPay + Allowance;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "\nStaffName = " + StaffName +
+                   "\n_managerHourlyRate = " + _managerHourlyRate +
+                   "\nHoursWorked = " + HoursWorked +
+                   "\nBasicPay = " + BasicPay +
+                   "\nAllowance = " + Allowance +
+                   "\n\nTotalPay = " + TotalPay;
+        }
+    }
+}
